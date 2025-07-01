d99ff7c9f4567f5f53f825d8622c9a26131bbc13
henryle97 <lehoang2797@gmail.com>
2025-07-01 23:34:25 +0700

    feat: create document by file api

--- examples/datasets.py
+++ examples/datasets.py
@@ -6,7 +6,6 @@ import dotenv
 
 from dify_client import DifyClient, models
 
-
 dotenv.load_dotenv()
 
 DIFY_API_BASE = os.getenv("DIFY_API_BASE")
@@ -78,6 +77,45 @@ def create_document_by_text():
     print(response)
 
 
+def create_document_by_file():
+    rules = models.Rule(
+        pre_processing_rules=[
+            {
+                "id": "remove_extra_spaces",
+                "enabled": True,
+                "type": "remove_urls_emails",
+                "params": True,
+            }
+        ],
+        segmentation={
+            "separator": "\n\n",
+            "max_tokens": 1024,
+        },
+        parent_mode="full-doc",
+        subchunk_segmentation={
+            "separator": "sentence",
+            "max_tokens": 1024,
+            "chunk_overlap": 50,
+        },
+    )
+    request = models.CreateDocumentByFileRequest(
+        indexing_technique=models.IndexModel.HIGH_QUALITY.value,
+        doc_form=models.DocForm.TEXT_MODEL.value,
+        process_rule=models.ProcessRule(
+            mode=models.SegmentationMode.CUSTOM.value,
+            rules=rules.model_dump(),
+        ),
+    )
+    print(request.model_dump())
+    file_path = "data/expo_presentation_iori.jp.txt"
+    response = client.create_document_by_file(
+        dataset_id="97937f3c-f2c6-4c4b-b266-f1b22a509b4e",
+        req=request,
+        file=file_path,
+    )
+    print(response)
+
+
 def get_documents():
     response = client.get_documents(
         dataset_id="894f6555-f3a6-43a0-9891-579cd64beaa8",
@@ -130,4 +168,5 @@ if __name__ == "__main__":
     # get_documents()
     # get_metadata_list()
     # update_document_metadata()
-    create_document_metadata()
+    # create_document_metadata()
+    create_document_by_file()
